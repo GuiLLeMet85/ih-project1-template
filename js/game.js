074026815683f79da5ff7a54b576d9815c3dbaeb
Guillem B <guillembaracco@gmail.com>
2022-05-10 20:37:05 +0200

    in class

--- js/game.js
+++ js/game.js
@@ -1,18 +1,22 @@
 class Game {
-    constructor() {
+    constructor(learnPage,solvePage) {      
+    // pensar quines propietats s'hauran de guardar.
+}
 
-    }
+//_hideLearningPage // set timeout en 5 s i console.log 
+    _hideLearningPage() {
+        setTimeout(() => {this._showSolvingPage()}, 5000);
+} 
 
-    _assignControls() {
+// seleccionar pagina que amagarem
+// seleccionar pagina que mostrarem
+    _showSolvingPage(learnPage, solvePage) {
+     
+    } 
 
-    }
-
-    _update() {
-        window.requestAnimationFrame(() => this._update());
-    }
+//_showRandomElement // triar aleatoriament una de les imatges de mapes i returnar un console.log de la imatge. S'ha de guardar totes les imatges en un array.
 
     start() {
-        this._assignControls();
-        this._update();
+        this._hideLearningPage();
     }
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,14 @@
 window.onload = function() {
     const startPage = document.getElementById('start-page');
     const startButton = document.getElementById('start');
-    const europeMap = document.getElementById('europe-page');
-    const europeMapNoName= document.getElementById('europe-page-noname');
+    const learnPage = document.getElementById('learn-page');
+    const solvePage = document.getElementById('solve-page');
 
     startButton.onclick = function() {
         startPage.style = "display: none";
-        europeMap.style = "display: flex";
-        europeMapNoName.style = "display: none";
-        const game = new Game();
+        learnPage.style = "display: flex";
+        solvePage.style = "display: none";
+        const game = new Game(learnPage,solvePage);
         game.start();
     }
 }
-
-
-class CountdownFirstMap {
-    constructor() {
-        this.currentTime = 0;
-        this.intervalId = null;
-    }
-
-    start() {
-        this.intervalId = setInterval(this.currentTime-=1,1000);
-    }
-    
-    getSeconds() {
-        return Math.floor(this.currentTime%60);
-    }
-
-    computeTwoDigitNumber(value) {
-        const Number = value.toString()
-        if(Number.length === 1 ){
-          return `0${Number}`
-        } else {
-          return Number
-        }
-      }
-
-    split() {
-        const seconds = this.computeTwoDigitNumber(this.getSeconds())
-        return `${seconds}`
-      }
-
-} 
