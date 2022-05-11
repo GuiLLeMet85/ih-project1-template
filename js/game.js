2b0e20601359a8e490376f7ca756042590858b8e
Guillem B <guillembaracco@hotmail.com>
2022-05-11 15:34:17 +0200

    In process..._showRandomElement

--- js/game.js
+++ js/game.js
@@ -1,37 +1,36 @@
 class Game {
-    constructor() {      
+    constructor() {
         // pensar quines propietats s'hauran de guardar.
-}
+    }
 
-//_hideLearningPage // set timeout en 5 s i console.log 
+    //_hideLearningPage // set timeout en 5 s i console.log 
     _hideLearningPage() {
-        setTimeout(() => {this._showSolvingPage()}, 3000);
-} 
+        setTimeout(() => { this._showSolvingPage() }, 3000);
+    }
 
-// seleccionar pagina que amagarem
-// seleccionar pagina que mostrarem
+    // seleccionar pagina que amagarem i seleccionar pagina que mostrarem
     _showSolvingPage() {
         this._hideLearningPage();
         const learnPage = document.getElementById('learn-page');
         learnPage.style = "display: none";
         const solvePage = document.getElementById('solve-page');
         solvePage.style = "display: flex";
-        console.log("Hola")
-} 
+    }
 
-//S'ha de guardar totes les imatges en un array.
-//_showRandomElement // triar aleatoriament una de les imatges de mapes i returnar un console.log de la imatge. 
+    //S'ha de guardar totes les imatges en un array.
+    //_showRandomElement // triar aleatoriament una de les imatges de mapes i returnar un console.log de la imatge. 
 
     _showRandomElement() {
         this._showSolvingPage();
-      img = new Array()
-            img[0] = '/img/france.png';
-            img[1] = '/img/norway.png';
-            img[2] = '/img/sweden.png';
-            console.log("Que tal")
-        ran = Math.floor(3 * Math.random());
-        document.write('<img src= " " +img[ran] +  " ">');
-}
+        const sign = prompt("Hola que hase");
+        console.log("Hola que hase");
+        /* img = new Array()
+         img[0] = '/img/france.png';
+         img[1] = '/img/norway.png';
+         img[2] = '/img/sweden.png';
+         ran = Math.floor(3 * Math.random()); */
+
+    }
 
     start() {
         this._hideLearningPage();
